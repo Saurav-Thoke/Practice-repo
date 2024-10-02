75e9780db2f6b7611bbe64ada377f1ee2942ef21
Saurav Thoke <sauravthoke28@gmail.com>
2024-10-02 11:46:00 +0530

    First commit

---
+++ Practice-tools/src/test/java/com/saurav/Practice_tools/PracticeToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saurav.Practice_tools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

0abf038ab7832c95559dfa7edb6c641dcf6b5c3e
Saurav Thoke <sauravthoke28@gmail.com>
2024-10-02 12:14:34 +0530

    Service method updated

--- Practice-tools/src/main/java/com/saurav/Practice_tools/ServiceClass.java
+++ Practice-tools/src/main/java/com/saurav/Practice_tools/ServiceClass.java
@@ -4,7 +4,9 @@ public class ServiceClass {
 
     public void getGreeting()
     {
+
         System.out.println("Hello every one....");
         System.out.println("Good Morning");
+        System.out.println("Have a nice Day");
     }
 }
